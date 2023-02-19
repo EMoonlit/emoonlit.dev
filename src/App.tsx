2d97537a29d21dad6212b5d4ae6bbbf1c64d76b3
Emerson Junqueira <emoonlit@gmail.com>
2023-02-19 16:55:02 -0300

    feat: add social link section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
+      <SocialLinks/>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,7 +30,7 @@ const NavBar = () => {
     ]
 
     return (
-        <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+        <section className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
           <div>
             <h1 className="text-5xl font-bold ml-2">EMoonlit</h1>
           </div>
@@ -74,7 +74,7 @@ const NavBar = () => {
               ))}
             </ul>
           )}
-        </div>
+        </section>
       );
     };
 
